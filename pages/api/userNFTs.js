db1f1986503ee3e6321180c46a759403b226e2f4
jonyz <jvalente96@gmail.com>
2022-09-21 21:24:18 +0100

    userntfs

--- pages/api/userNFTs.js
+++ pages/api/userNFTs.js
@@ -55,7 +55,7 @@ export default async function handler(req, res) {
   ).catch((err) => console.error(err));
   const nfts = await getNFTsResponse.json();
 
-  console.log("nfts", nfts);
+  console.log("nfts", nfts.ownedNfts);
 
-  res.status(200).json(nfts);
+  res.status(200).json(nfts.ownedNfts);
 }
