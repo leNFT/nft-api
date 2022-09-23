5aec29d78e11e000882cc0197ff72fb4366ce8d2
jonyz <jvalente96@gmail.com>
2022-09-23 19:10:11 +0100

    change test asset valuation

--- pages/api/getAssetValuation.js
+++ pages/api/getAssetValuation.js
@@ -12,7 +12,7 @@ export async function getAssetValuation(collection, tokenId) {
 
   // Test collection case for goerli
   if (collection == "0x0171dB1e3Cc005d2A6E0BA531509D007a5B8C1a8") {
-    return parseUnits("0.002", 18);
+    return parseUnits("0.0018", 18);
   }
 
   const url =
