ba15003168e25cefc7bfe92daa304e7600e0b4ce
jonyz <jvalente96@gmail.com>
2022-10-10 22:58:39 +0100

    change test asset valuation

--- pages/api/getAssetValuation.js
+++ pages/api/getAssetValuation.js
@@ -12,7 +12,7 @@ export async function getAssetValuation(collection, tokenId) {
 
   // Test collection case for goerli
   if (collection == "0x0171dB1e3Cc005d2A6E0BA531509D007a5B8C1a8") {
-    return parseUnits("0.005", 18);
+    return parseUnits("0.0048", 18);
   }
 
   const url = "https://api.upshot.xyz/v2/assets/" + collection + "/" + tokenId;
