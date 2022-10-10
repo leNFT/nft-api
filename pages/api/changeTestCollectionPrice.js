225b45e1cf4700b6cc60217e3891d2f35931a763
jonyz <jvalente96@gmail.com>
2022-10-10 22:48:59 +0100

    fix path2

--- pages/api/changeTestCollectionPrice.js
+++
@@ -1,8 +0,0 @@
-export async function changeTestCollectionPrice(newPrice) {
-  // read file and make object
-  let content = JSON.parse(fs.readFileSync("testData.json", "utf8"));
-  // edit or add property
-  content.price = newPrice;
-  //write file
-  fs.writeFileSync("testData.json", JSON.stringify(content));
-}
--- pages/api/getAssetValuation.js
+++ pages/api/getAssetValuation.js
@@ -1,6 +1,5 @@
 import fetch from "node-fetch";
 import { parseUnits } from "@ethersproject/units";
-import testData from "./testData.json";
 
 export async function getAssetValuation(collection, tokenId) {
   const options = {
@@ -12,8 +11,8 @@ export async function getAssetValuation(collection, tokenId) {
   };
 
   // Test collection case for goerli
-  if (collection == testData.collection) {
-    return parseUnits(testData.price, 18);
+  if (collection == "0x0171dB1e3Cc005d2A6E0BA531509D007a5B8C1a8") {
+    return parseUnits("0.005", 18);
   }
 
   const url = "https://api.upshot.xyz/v2/assets/" + collection + "/" + tokenId;
