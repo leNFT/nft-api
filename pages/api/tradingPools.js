4500f8683083b7a2243bc4297d0ff5d1ae934fbc
jvalente <jvalente96@gmail.com>
2022-12-18 22:53:30 +0000

    added trading pools api

---
+++ pages/api/tradingPools.js
@@ -0,0 +1,65 @@
+import Cors from "cors";
+import initMiddleware from "../../lib/init-middleware";
+import { utils } from "ethers";
+import { BigNumber } from "@ethersproject/bignumber";
+import contractAddresses from "../../contractAddresses.json";
+import { Network, Alchemy } from "alchemy-sdk";
+
+// Initialize the cors middleware
+const cors = initMiddleware(
+  // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+  Cors({
+    // Only allow requests with GET and from the frontend
+    methods: ["GET"],
+    origin: [
+      "https://lenft.finance",
+      "http://localhost:3000",
+      "https://lenft.fi",
+    ],
+  })
+);
+
+export default async function handler(req, res) {
+  // Run cors
+  await cors(req, res);
+
+  const { chainId } = req.query;
+  const createTradingPoolTopic =
+    "0x62bc44df7dccd51a32794da404454232658add2bef839a8e3629ed5c980daabf";
+
+  const alchemySettings = {
+    apiKey: process.env.ALCHEMY_API_KEY,
+    network: chainId == 1 ? Network.ETH_MAINNET : Network.ETH_GOERLI,
+  };
+  const alchemy = new Alchemy(alchemySettings);
+
+  const addresses =
+    chainId in contractAddresses
+      ? contractAddresses[chainId]
+      : contractAddresses["1"];
+
+  var tradingPools = {};
+
+  const tradingPoolsResponse = await alchemy.core.getLogs({
+    address: addresses.TradingPoolFactory,
+    fromBlock: "earliest",
+    toBlock: "latest",
+    topics: [createTradingPoolTopic],
+  });
+
+  try {
+    // Go through each event
+    tradingPoolsResponse.forEach((element) => {
+      tradingPools[
+        utils.defaultAbiCoder.decode(["address"], element.topics[1])
+      ] = {
+        nft: utils.defaultAbiCoder.decode(["address"], element.topics[2]),
+        token: utils.defaultAbiCoder.decode(["address"], element.topics[3]),
+      };
+    });
+  } catch (error) {
+    console.log(error);
+  }
+
+  res.status(200).json(tradingPools);
+}
